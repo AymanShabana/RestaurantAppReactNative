b0745f0874418e978079c5a5fc8a435572139ee6
Ayman Shabana <53878617+AymanShabana@users.noreply.github.com>
2020-08-10 23:28:40 +0200

    initial customization

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Yoooooooooooo from Ayman!!</Text>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:'#cef',
+    justifyContent: "center",
+    alignItems: "center",  
+  },
+});
