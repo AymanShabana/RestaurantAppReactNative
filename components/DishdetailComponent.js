92e55528a5d9b91016471ed7cbef107fff8f71e2
Ayman Shabana <53878617+AymanShabana@users.noreply.github.com>
2020-08-12 22:30:58 +0200

    added favorites

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -3,13 +3,19 @@ import { View, Text, ScrollView, FlatList } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import {connect} from 'react-redux';
 import {baseUrl} from '../shared/baseUrl';
-
+import { postFavorite} from '../redux/ActionCreators'
 const mapStateToProps = state => {
     return {
         dishes: state.dishes,
-        comments: state.comments
+        comments: state.comments,
+        favorites: state.favorites
     }
 }
+
+const mapDispatchToProps = dispatch => ({
+    postFavorite: (dishId) => dispatch(postFavorite(dishId))
+})
+
 function RenderDish(props){
     const dish = props.dish;
 
@@ -45,15 +51,10 @@ function RenderComments(props){
 }
 
 class Dishdetail extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            favorites: []
-        };
-    }
+
 
     markFavorite(dishId){
-        this.setState({favorites: this.state.favorites.concat(dishId)});
+        this.props.postFavorite(dishId);
     }
     static navigationOptions = {
         title: 'Dish Details'
@@ -63,11 +64,11 @@ class Dishdetail extends Component{
         const dishId = this.props.navigation.getParam('dishId', '');
         return(
             <ScrollView>
-                <RenderDish dish={this.props.dishes.dishes[+dishId]} favorite={this.state.favorites.some(el => el===dishId)} onPress={() =>this.markFavorite(dishId)}/>
+                <RenderDish dish={this.props.dishes.dishes[+dishId]} favorite={this.props.favorites.some(el => el===dishId)} onPress={() =>this.markFavorite(dishId)}/>
                 <RenderComments comments={this.props.comments.comments.filter((comment)=>comment.dishId === dishId)}/>
             </ScrollView>
         );
 
     }
 }
-export default connect(mapStateToProps)(Dishdetail);
+export default connect(mapStateToProps, mapDispatchToProps)(Dishdetail);
